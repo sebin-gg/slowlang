3c1252a9ea910f1bd239415f0371b7dd26c5f5cd
Sebin Mathew <69630250+sebin-gg@users.noreply.github.com>
2025-08-09 00:31:25 +0530

    Add turtle rage ASCII feedback on speed error

---
+++ ascii_turtle.py
@@ -0,0 +1,10 @@
+def show_turtle_rage():
+    turtle_art = r'''
+             _____     ____
+           /      \  |  o | 
+          |        |/ ___\| 
+          |_________/     
+          |_|_| |_|_|
+    🐢 THE TURTLE IS DISAPPOINTED.
+    '''
+    print(turtle_art)
