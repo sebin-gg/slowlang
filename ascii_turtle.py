35788a9fd5cf41fed753cf7fe43f4962b4f4274f
Sebin Mathew <69630250+sebin-gg@users.noreply.github.com>
2025-08-09 04:28:10 +0530

    to fix

--- ascii_turtle.py
+++ ascii_turtle.py
@@ -11,3 +11,29 @@ def show_turtle_rage():
     🐢 RAGE MODE: Turtle is not amused by your speed.
     """
     print(turtle)
+
+def show_turtle_just_right():
+    turtle = r"""
+         _____
+       < ^   ^ >
+        \  -  /
+        /     \
+       |  o o  |
+       |   ∆   |
+       \_______/
+    🐢 Turtle is content with your pace.
+    """
+    print(turtle)
+
+def show_turtle_too_slow():
+    turtle = r"""
+         _____
+       < -   - >
+        \  _  /
+        /     \
+       |  . .  |
+       |   -   |
+       \_______/
+    🐢 Turtle is falling asleep. Type a bit faster!
+    """
+    print(turtle)
